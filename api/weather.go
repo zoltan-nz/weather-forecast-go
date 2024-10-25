8cf2dbeb9b4a7039fdaa795ae65fcd41dd494ba6
Zoltan <zdebre@gmail.com>
2024-10-25 03:18:29 -0400

    feat(geo-coding): improve geo data formats

--- api/weather.go
+++ api/weather.go
@@ -7,19 +7,25 @@ import (
 	"net/http"
 )
 
+// OpenMeteoGeocodingApiUrl
 // API Documentation: https://open-meteo.com/en/docs/geocoding-api
 const OpenMeteoGeocodingApiUrl = "https://geocoding-api.open-meteo.com/v1/search"
 
 type GeoResponse struct {
-	Results []LatLong `json:"results"`
+	Results []LatLongResponse `json:"results"`
 }
 
-type LatLong struct {
+type LatLongResponse struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
 
-func FetchLatLong(city string) (float64, float64, error) {
+type LatLong struct {
+	Lat  float64
+	Long float64
+}
+
+func FetchLatLong(city string) (LatLong, error) {
 	client := resty.New()
 	queryParams := map[string]string{
 		"name":     city,
@@ -30,21 +36,21 @@ func FetchLatLong(city string) (float64, float64, error) {
 	response, err := client.R().SetQueryParams(queryParams).Get(OpenMeteoGeocodingApiUrl)
 
 	if err != nil {
-		return 0, 0, fmt.Errorf("error making request to OpenMeteoGeocodingApi: %w", err)
+		return LatLong{}, fmt.Errorf("error making request to OpenMeteoGeocodingApi: %w", err)
 	}
 
 	if response.StatusCode() != http.StatusOK {
-		return 0, 0, fmt.Errorf("unexpected status code: %d and response: %s", response.StatusCode(), response.String())
+		return LatLong{}, fmt.Errorf("unexpected status code: %d and response: %s", response.StatusCode(), response.String())
 	}
 
 	var geoResponse GeoResponse
 	if err := json.Unmarshal(response.Body(), &geoResponse); err != nil {
-		return 0, 0, fmt.Errorf("error decoding response: %w", err)
+		return LatLong{}, fmt.Errorf("error decoding response: %w", err)
 	}
 
 	if len(geoResponse.Results) == 0 {
-		return 0, 0, fmt.Errorf("no results found for city: %s", city)
+		return LatLong{}, fmt.Errorf("no results found for city: %s", city)
 	}
 
-	return geoResponse.Results[0].Latitude, geoResponse.Results[0].Longitude, nil
+	return LatLong{geoResponse.Results[0].Latitude, geoResponse.Results[0].Longitude}, nil
 }
--- api/weather_test.go
+++ api/weather_test.go
@@ -4,42 +4,44 @@ import (
 	"testing"
 )
 
+// FetchLatLong is a function that fetches the latitude and longitude of a city
+// using the OpenMeteoGeocoding API.
+// It should return the latitude and longitude objects (LatLong) if the city is found.
 func TestFetchLatLong(t *testing.T) {
 	tests := []struct {
-		name     string
-		city     string
-		wantLat  float64
-		wantLong float64
-		wantErr  bool
+		name    string
+		city    string
+		want    LatLong
+		wantErr bool
 	}{
 		{
-			name:     "Valid city",
-			city:     "London",
-			wantLat:  51.50853,
-			wantLong: -0.12574,
-			wantErr:  false,
+			name:    "Valid city",
+			city:    "London",
+			want:    LatLong{51.50853, -0.12574},
+			wantErr: false,
 		},
 		{
-			name:     "Invalid city",
-			city:     "NonExistentCity",
-			wantLat:  0,
-			wantLong: 0,
-			wantErr:  true,
+			name:    "Invalid city",
+			city:    "NonExistentCity",
+			want:    LatLong{0, 0},
+			wantErr: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			lat, long, err := FetchLatLong(tt.city)
+			{
+			}
+			latLong, err := FetchLatLong(tt.city)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("FetchLatLong() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if lat != tt.wantLat {
-				t.Errorf("FetchLatLong() lat = %v, wantLat %v", lat, tt.wantLat)
+			if latLong.Lat != tt.want.Lat {
+				t.Errorf("FetchLatLong() lat = %v, wantLat %v", latLong.Lat, tt.want.Lat)
 			}
-			if long != tt.wantLong {
-				t.Errorf("FetchLatLong() long = %v, wantLong %v", long, tt.wantLong)
+			if latLong.Long != tt.want.Long {
+				t.Errorf("FetchLatLong() long = %v, wantLong %v", latLong.Long, tt.want.Long)
 			}
 		})
 	}
